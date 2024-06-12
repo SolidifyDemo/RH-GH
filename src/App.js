26fdff80778c35f89b2135fe1cd9a4668daaaeff
Rickard Hole Falck <rickard.hole.falck@solidify.dev>
2024-06-12 11:55:41 +0200

    added input

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
+  const header = process.env.REACT_APP_HEADER || 'Default Header';
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TODO thingy</h1>
+        <h1>{header}</h1>
         <TodoList />
       </header>
     </div>
